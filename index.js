0efa9901b6dc94d798e71be9076827855a226320
jorgeruuiz <jorgeruiz2002@hotmail.es>
2025-02-01 13:29:35 +0100

    servidor de prueba

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const express = require('express');
+const socket = require('socket.io');
+const http = require('http');
+const {Chess} = require('chess.js');
+
+const app = express();
+
+// Crea el servidor manualmente para poder utilizar WebSockets
+const server = http.createServer(app);
+const io = socket(server);
+
+const PORT = 3000;
+
+// Para poder recibir JSONs en el body de las requests
+app.use(express.json());                                                 
+
+// Ruta de prueba para comprobar que el servidor está funcionando
+app.get("/", (req, res) => {
+  res.json({ message: "Servidor funcionando" });
+});
+
+// Inicializa el servidor, que se pone a la escucha
+server.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  });
+  
+// WebSockets para comunicación en tiempo real
+io.on("connection", (socket) => {
+  console.log("Un usuario se ha conectado");
+
+  socket.on("disconnect", () => {
+    console.log("Un usuario se ha desconectado");
+  });
+});
+
+
